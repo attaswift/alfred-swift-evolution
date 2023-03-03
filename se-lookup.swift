722be3cd64a629afdcf455e2e594f7bf3b5de1e5
Ole Begemann <ole@oleb.net>
2023-03-03 12:25:00 +0100

    Fix URLs, modernize code (Codable, Swift 5), better error reporting
    This updates the hardcoded URLs to make the script work again.
    
    The main change is to switch to Codable for decoding the JSON downloaded from data.swift.org and for encoding the JSON response to Alfred.
    
    To decouple the actual JSON formats used by the source (swift.org) and the destination (Alfred), this introduces two new structs, ProposalDTO and AlfredItem. These model the data formats we need to work with faithfully. The Proposal struct still exists as the currency type for our own business logic. So the program now works like this:
    
    Download and parse [ProposalDTO] → Convert to [Proposal] and process → Convert to [AlfredItem] and return
    
    The new structs mirror the data formats 1:1, which makes them a good piece of documentation. It should also be easy to support more fields, both in the input and in the output format.

--- se-lookup.swift
+++ se-lookup.swift
@@ -6,31 +6,91 @@
 
 import Foundation
 
-let baseURL = URL(string: "https://github.com/apple/swift-evolution/blob/master/proposals")!
-let dataURL = URL(string: "https://data.swift.org/swift-evolution/proposals")!
+/// Data transfer object definition for a Swift Evolution proposal in the
+/// JSON format used by swift.org.
+struct ProposalDTO: Decodable {
+    static let dataURL = URL(string: "https://download.swift.org/swift-evolution/proposals.json")!
 
-struct LookupError: LocalizedError {
-    let errorDescription: String?
+    var id: String
+    var title: String
+    var link: String
+    var status: Status
 
-    init(_ message: String) {
-        self.errorDescription = message
+    struct Status: Decodable {
+        var state: String
     }
 }
 
 struct Proposal {
-    enum Status: String, CustomStringConvertible {
-        case awaitingReview = ".awaitingReview"
-        case scheduledForReview = ".scheduledForReview"
-        case activeReview = ".activeReview"
-        case returnedForRevision = ".returnedForRevision"
-        case withdrawn = ".withdrawn"
-        case deferred = ".deferred"
-        case accepted = ".accepted"
-        case acceptedWithRevisions = ".acceptedWithRevisions"
-        case rejected = ".rejected"
-        case implemented = ".implemented"
-        case error = ".error"
-        case unknown = ""
+    let baseURL = URL(string: "https://github.com/apple/swift-evolution/blob/main/proposals")!
+
+    var id: String
+    var title: String
+    var url: URL
+    var status: Status
+
+    var number: Int? {
+        guard let digits = id.split(separator: "-").last else { return nil }
+        return Int(digits)
+    }
+
+    var searchText: String {
+        "\(id) \(number.map(String.init(describing:)) ?? "") \(title) \(status.description) \(status.description)"
+            .lowercased()
+    }
+
+    func matches(_ query: String) -> Bool {
+        let words = query
+            .split { $0.isWhitespace || $0.isNewline }
+            .map { $0.lowercased() }
+        if words.count == 0 { return true }
+        if words.count == 1, let number = Int(words[0]) {
+            return self.number == number
+        }
+        return words.contains { searchText.contains($0) }
+    }
+}
+
+extension Proposal {
+    init(dto: ProposalDTO) {
+        self.id = dto.id
+        self.title = dto.title.trimmingCharacters(in: .whitespacesAndNewlines)
+        self.url = baseURL.appendingPathComponent(dto.link)
+        self.status = Status(dto: dto.status)
+    }
+}
+
+extension Proposal {
+    enum Status: CustomStringConvertible {
+        case awaitingReview
+        case scheduledForReview
+        case activeReview
+        case returnedForRevision
+        case withdrawn
+        case deferred
+        case accepted
+        case acceptedWithRevisions
+        case rejected
+        case implemented
+        case error
+        case unknown(status: String)
+
+        init(dto: ProposalDTO.Status) {
+            switch dto.state {
+            case ".awaitingReview": self = .awaitingReview
+            case ".scheduledForReview": self = .scheduledForReview
+            case ".activeReview": self = .activeReview
+            case ".returnedForRevision": self = .returnedForRevision
+            case ".withdrawn": self = .withdrawn
+            case ".deferred": self = .deferred
+            case ".accepted": self = .accepted
+            case ".acceptedWithRevisions": self = .acceptedWithRevisions
+            case ".rejected": self = .rejected
+            case ".implemented": self = .implemented
+            case ".error": self = .error
+            default: self = .unknown(status: dto.state)
+            }
+        }
 
         var description: String {
             switch self {
@@ -45,85 +105,106 @@ struct Proposal {
             case .rejected: return "Rejected"
             case .implemented: return "Implemented"
             case .error: return "Error"
-            case .unknown: return "Unknown Status"
+            case .unknown(let underlying): return "Unknown status: \(underlying)"
             }
         }
     }
+}
 
-    let number: Int
-    let id: String
-    let title: String
-    let url: URL
-    let status: Status
-    let searchText: String
-
-    init?(from json: Any) {
-        guard let json = json as? [String: Any] else { return nil }
-        guard json["errors"] == nil else { return nil }
-        guard let id = json["id"] as? String else { return nil }
-        guard id.hasPrefix("SE-") else { return nil }
-        guard let number = Int(id.components(separatedBy: "-").last!) else { return nil }
-        guard let title = json["title"] as? String else { return nil }
-        guard let link = json["link"] as? String else { return nil }
-        self.number = number
-        self.id = id
-        self.title = title.trimmingCharacters(in: .whitespacesAndNewlines)
-        self.status = Status(rawValue: (json["status"] as? [String: Any])?["state"] as? String ?? "") ?? .unknown
-        self.url = baseURL.appendingPathComponent(link)
-
-        self.searchText = "\(id) \(number) \(title) \(status.rawValue) \(status.description)".lowercased()
+/// An item a Script Filter workflow returns to Alfred for display.
+/// Represents one row in an Alfred result set.
+///
+/// Documentation: <https://www.alfredapp.com/help/workflows/inputs/script-filter/json/>
+struct AlfredItem: Encodable {
+    enum `Type`: String, Encodable {
+        /// An item that is not a file.
+        case `default`
+        /// "type": "file" makes Alfred treat your result as a file on your system. This allows the
+        /// user to perform actions on the file like they can with Alfred's standard file filters.
+        case file
+        /// When returning files, Alfred will check if the file exists before presenting that result
+        /// to the user. This has a very small performance implication but makes the results as
+        /// predictable as possible. If you would like Alfred to skip this check as you are certain
+        /// that the files you are returning exist, you can use "type": "file:skipcheck".
+        case fileSkipCheck = "file:skipcheck"
     }
 
-    var alfredItem: [String: String] {
-        return [
-            "uid": url.absoluteString,
-            "type": "default",
-            "title": "\(id)",
-            "subtitle": "\(title) (\(status.description))",
-            "autocomplete": id,
-            "quicklookurl": url.absoluteString,
-            "arg": url.absoluteString,
-        ]
+    /// A unique identifier for the item which allows Alfred to learn about this item for subsequent
+    /// sorting and ordering of the user's actioned results.
+    ///
+    /// If you would like Alfred to always show the results in the order you return them from your
+    /// script, exclude the UID field.
+    var uid: String?
+    /// The title displayed in the result row.
+    var title: String
+    /// The subtitle displayed in the result row.
+    var subtitle: String?
+    /// The argument which is passed through the workflow to the connected output action.
+    /// Optional, but strongly recommended.
+    var arg: String?
+    /// Whether this item is valid or not. If an item is valid then Alfred will action this item
+    /// when the user presses return. If the item is not valid, Alfred will do nothing.
+    var valid: Bool = true
+    /// An optional but recommended string you can provide which is populated into Alfred's search
+    /// field if the user auto-completes (Tab key by default) the selected result.
+    var autocomplete: String?
+    /// The item type.
+    var type: `Type` = .default
+    /// A Quick Look URL which will be visible if the user uses the Quick Look feature within Alfred
+    /// (tapping Shift, or Cmd+Y). Note that quicklookurl will also accept a file path, both
+    /// absolute and relative to home using ~/.
+    var quicklookurl: String?
+}
+
+extension AlfredItem {
+    init(proposal: Proposal) {
+        self.uid = proposal.url.absoluteString
+        self.title = proposal.id
+        self.subtitle = "\(proposal.title) (\(proposal.status.description))"
+        self.arg = proposal.url.absoluteString
+        self.autocomplete = proposal.id
+        self.quicklookurl = proposal.url.absoluteString
     }
 
-    func matches(_ query: String) -> Bool {
-        let words = query
-            .components(separatedBy: .whitespacesAndNewlines)
-            .filter { !$0.isEmpty }
-            .map { $0.lowercased() }
-        if words.count == 0 { return true }
-        if words.count == 1, let number = Int(words[0]) {
-            return self.number == number
-        }
-        return words.index { !searchText.contains($0) } == nil
+    init(error: Error) {
+        // Dumping an `Error`’s contents seems to be the best way to extract all the
+        // salient error information into a semi-readable string. This obviously isn’t
+        // ideal for user-facing error messages, but I think it’s acceptable for a
+        // developer tool such as this.
+        // Unfortunately, `error.localizedDescription` or the various `LocalizedError` 
+        // properties carry little to no actionable information about the failure reason
+        // for typical library errors such as Foundation.CocoaError or Swift.DecodingError.
+        let title = "Error: \(error.localizedDescription)"
+        var errorInfo = ""
+        dump(error, to: &errorInfo)
+        self.init(
+            uid: "status", // Identifier for status messages
+            title: title,
+            subtitle: errorInfo,
+            arg: "\(title)\n\(errorInfo)",
+            valid: false
+        )
     }
 }
+
+// MARK: - Main program
 
 let query = CommandLine.arguments.dropFirst().joined(separator: " ")
-
+let result: [AlfredItem]
 do {
-    guard let data = try? Data(contentsOf: dataURL) else {
-        throw LookupError("Can't download proposals data")
-    }
-    guard let json = try JSONSerialization.jsonObject(with: data) as? [Any] else {
-        throw LookupError("Proposal data has unknown structure")
-    }
-    let items = json
-        .flatMap { Proposal(from: $0) }
+    let data = try Data(contentsOf: ProposalDTO.dataURL)
+    let decoder = JSONDecoder()
+    let allProposals = try decoder.decode([ProposalDTO].self, from: data)
+    result = allProposals
+        .map(Proposal.init(dto:))
         .filter { $0.matches(query) }
-        .sorted { $0.number > $1.number }
-        .map { $0.alfredItem }
+        .sorted { ($0.number ?? 0) > ($1.number ?? 0) }
+        .map(AlfredItem.init(proposal:))
+} catch {
+    result = [AlfredItem(error: error)]
+}
 
-    let resultData = try JSONSerialization.data(withJSONObject: ["items": items], options: .prettyPrinted)
-    print(String(data: resultData, encoding: .utf8)!)
-}
-catch {
-    let report: [String: Any] = [
-        "valid": false,
-        "type": "default",
-        "title": "Swift proposals are unavailable",
-        "subtitle": "\(error.localizedDescription)",
-    ]
-    let resultData = try JSONSerialization.data(withJSONObject: ["items": [report]], options: .prettyPrinted)
-    print(String(data: resultData, encoding: .utf8)!)
-}
+let encoder = JSONEncoder()
+encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
+let resultData = try encoder.encode(["items": result])
+print(String(decoding: resultData, as: UTF8.self))
